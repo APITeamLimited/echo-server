f5bbae795e5140021f1db9cf5d65a95964a573ee
Harry Twigg <harrytwigg111@gmail.com>
2022-11-11 12:44:44 +0000

    Build script fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t echo-server:latest .
-docker build -t echo-server-gateway:latest . -f Dockerfile.gateway
+docker build -t apiteamdevops/echo-server:latest .
+docker build -t apiteamdevops/echo-server-gateway:latest . -f Dockerfile.gateway
 
 docker push apiteamdevops/echo-server:latest
 docker push apiteamdevops/echo-server-gateway:latest
